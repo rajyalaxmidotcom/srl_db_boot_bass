fdf5d3948ae50bf57989bae93056c27e34b1a6a4
Abhi <Abhi@DESKTOP-M3Q06NL>
2024-07-17 21:53:27 +0530

    first commit

---
+++ srl_db_boot_bass/src/main/java/com/srl/SrlDbBootBassApplication.java
@@ -0,0 +1,13 @@
+package com.srl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrlDbBootBassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrlDbBootBassApplication.class, args);
+	}
+
+}
---
+++ srl_db_boot_bass/src/main/java/com/srl/controller/vendorControllerNew.java
@@ -0,0 +1,23 @@
+package com.srl.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+/*import org.springframework.data.jpa.repository.JpaRepository;*/
+/*import org.springframework.data.rest.core.annotation.RepositoryRestResource;*/
+/*import org.springframework.data.rest.core.annotation.RepositoryRestResource;*/
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.srl.entity.Vendor;
+
+
+/*@RepositoryRestResource(collectionResourceRel="vendor", path="newVendor")*/
+@RepositoryRestResource(collectionResourceRel="vendor", path="newVendor")
+
+
+public interface vendorControllerNew extends JpaRepository<Vendor,Long> {
+	
+}
+
+
+
+
+
